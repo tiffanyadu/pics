53bf93762bdb5dffe6fca2e547d6ce61293df054
tiffanyadu <tadofdesign@gmail.com>
2019-02-18 16:00:12 -0600

    Create GET request to Unsplash API

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,13 @@ import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID b5c8f23cd8a86177640c36808c455e9302b818c51705ffd151557e7bb74dc3e5"
+      }
+    });
   }
   render() {
     return (
